3c92808c453b9a9ab36ca22a8ca96b48a560aa17
Baljinder-96 <57005915+Baljinder-96@users.noreply.github.com>
2020-04-01 19:34:48 -0400

    Add files via upload

---
+++ financing.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Prime Properties :: Financing</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="prime.css">
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <!--[if lt IE 9]>
+        <script src=" http://html5shim.googlecode.com/svn/trunk/html5.js ">
+        </script>
+    <![endif]-->
+</head>
+
+<body>
+     <div id="container-fluid">
+    <div id="wrapper">
+        <div class="well" style="background-color: lightgrey;">
+        <header>
+            <h1>Prime Properties</h1>
+        </header>
+            </div>
+        <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#"><div class="prime-properties">Prime</div></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li><a href="index.php"><span class="glyphicon glyphicon-home"></span>&nbsp;&nbsp;Home</a></li>
+                <li><a href="listings.php"><span class="glyphicon glyphicon-star-empty"></span>&nbsp;&nbsp;Listings</a></li>
+                <li><a href="financing.php"><span class="glyphicon glyphicon-usd"></span>&nbsp;&nbsp;Financing</a></li>
+              </ul>
+             </div>
+            </div>
+           </nav>
+    
+        <main>
+
+            <h2>Financing</h2>
+            <p>We work with many area mortgage and finance companies.</p>
+            <h3>Mortgage FAQs</h3>
+            <div class="jumbotron" style="text-align: center;">
+            <dl>
+                <dt>What amount of mortgage do I qualify for? </dt>
+            <dd>The total basic monthly housing cost is normally based on 29% to 41% of your gross monthly income.</dd>
+                <dt>Which percentage is most often used? </dt>
+               <dd>The percentage used depends on the lending institution and type of financing.</dd>
+                <dt>How do I get started?</dt>
+             <dd> Contact us today to help you arrange financing for your home.</dd>
+                </dl>
+             </div>
+        </main>
+        <div class="well" style="background-color: lightgrey;">
+        <footer>
+            Copyright &copy; 2018 Prime Properties<br>
+            <a href="mailto:Baljinder@Kaur.com">Baljinder@kaur.com</a>
+        </footer>
+            </div>
+    </div>
+         </div>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Prime Properties</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="prime.css">
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <!--[if lt IE 9]>
+        <script src=" http://html5shim.googlecode.com/svn/trunk/html5.js ">
+        </script>
+    <![endif]-->
+
+</head>
+
+<body>
+    <div id="container-fluid">
+    <div id="wrapper">
+        <div class="well" style="background-color: lightgrey;">
+        <header>
+            <h1>Prime Properties</h1>
+        </header>
+            </div>
+        <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#"><div class="prime-properties">Prime</div></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li><a href="index.php"><span class="glyphicon glyphicon-home"></span>&nbsp;&nbsp;Home</a></li>
+                <li><a href="listings.php"><span class="glyphicon glyphicon-star-empty"></span>&nbsp;&nbsp;Listings</a></li>
+                <li><a href="financing.php"><span class="glyphicon glyphicon-usd"></span>&nbsp;&nbsp;Financing</a></li>
+              </ul>
+             </div>
+            </div>
+           </nav>
+    
+        <main>
+            <p class="text-success"><span class="glyphicon glyphicon-star-empty"></span>&nbsp;Prime Properties is prepared to market and sell your property.</p>
+            <p class="text-primary"><span class="glyphicon glyphicon-star-empty"></span>&nbsp;The philosophy of Prime Properties is to promote our clients, not ourselves. </p>
+            <p class="text-danger"><span class="glyphicon glyphicon-star-empty"></span>&nbsp;We can also help you find the property that meets your needs:</p>
+            <ul class="list-group" style="font-weight: bold;">
+                <li class="list-group-item list-group-item-default">location</li>
+                <li class="list-group-item list-group-item-default">price</li>
+                <li class="list-group-item list-group-item-default">features</li>
+            </ul>
+            <div class="contact">Prime Properties<br> 3055 Bode Road<br> Schaumburg, IL 60194<br>
+                
+                <a id="mobile" href="tel:847-555-5555">847-555-5555</a>
+            <span id="desktop">847-555-5555</span><br> <br>
+            </div>
+        </main>
+           <div class="well" style="background-color: lightgrey;">
+        <footer>
+            Copyright &copy; 2018 Prime Properties<br>
+            <a href="mailto:Baljinder@kaur.com">Baljinder@kaur.com</a>
+        </footer>
+               </div>
+    </div>
+        </div>
+</body>
+
+</html>
